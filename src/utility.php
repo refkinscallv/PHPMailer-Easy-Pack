2500d81f8a5c444ed5ffbc4f3fca453890ac1dd3
refkinscallv <refkinscallv@gmail.com>
2022-10-09 13:06:40 +0700

    Complete Function send_mail()

--- src/utility.php
+++ src/utility.php
@@ -95,7 +95,7 @@
                     if($bcc[1] == 0 || $bcc[1] == "0" || $bcc[1] == ""){
                         $mail->addBCC($bcc[0]);
                     } else {
-                        $mail->addBCC($bcc[0], $bcc[1])
+                        $mail->addBCC($bcc[0], $bcc[1]);
                     }
                 }
             }
